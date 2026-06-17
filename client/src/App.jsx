5aef52f828e0fdb892a4b5dcbeec17da5e4f638e
helmi <hbenzaied121@gmail.com>
2026-06-17 08:58:51 -0400

    Evaluation finaleH26

---
+++ client/src/App.jsx
@@ -0,0 +1,48 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+import Livres from "./pages/Livres";
+import MesEmprunts from "./pages/MesEmprunts";
+
+function App() {
+
+  return (
+    
+
+    <BrowserRouter>
+ <>
+<h1>Bibliothèque numérique : Ben Zaied Helmi</h1>
+</>
+      <nav>
+       
+        <Link to="/">
+          Livres
+        </Link>
+
+        {" | "}
+
+        <Link to="/emprunts">
+          Mes emprunts
+        </Link>
+
+      </nav>
+
+      <Routes>
+
+        <Route
+          path="/"
+          element={<Livres />}
+        />
+
+        <Route
+          path="/emprunts"
+          element={<MesEmprunts />}
+        />
+
+      </Routes>
+
+    </BrowserRouter>
+
+  );
+}
+
+export default App;
